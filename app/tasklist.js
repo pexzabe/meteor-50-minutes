c15ca24a37d03e34b838433f6669f2fab893cee8
Preston Exzabe <pexzabe@hawaii.edu>
2016-10-09 23:04:40 -1000

    Task 4: tasklist authentification

--- app/tasklist.js
+++ app/tasklist.js
@@ -15,10 +15,7 @@ if (Meteor.isClient) {
     "submit .add-task": function(event) {
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      })
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
 
@@ -27,7 +24,7 @@ if (Meteor.isClient) {
 
     "click .delete-task": function(event){
       if(confirm('Delete Task?')){
-        Tasks.remove(this._id);
+        Meteor.call('deleteTask', this._id);
       }
 
       return false;
@@ -38,3 +35,34 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function(name) {
+    if(!Meteor.userId()){
+      throw new Meteor.Error('No Access!');
+    }
+
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    })
+  },
+
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
